8690f5c342e00fe60c073a93e8449e7b71e3b240
Rituraj-commits <riturajdutta400@gmail.com>
2024-02-21 18:57:24 +0530

    Commit v4.2.0

--- src/main/java/com/adp/timeattendance/controller/TimeShiftController.java
+++ src/main/java/com/adp/timeattendance/controller/TimeShiftController.java
@@ -1,12 +1,15 @@
 package com.adp.timeattendance.controller;
 
 
+import com.adp.timeattendance.model.TimeRecord;
 import com.adp.timeattendance.model.TimeShift;
 import com.adp.timeattendance.repository.TimeShiftRepository;
 import com.adp.timeattendance.service.TimeShiftService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -18,7 +21,9 @@ public class TimeShiftController {
 
     @GetMapping("/{id}")
     public ResponseEntity<TimeShift> retrieveShift(@PathVariable Integer id){
-        return ResponseEntity.ok(timeShiftService.getShift(id));
+        TimeShift newTimeShift = timeShiftService.getShift(id);
+        if(newTimeShift!=null) return  ResponseEntity.ok(newTimeShift);
+        return ResponseEntity.notFound().build();
     }
 
 
